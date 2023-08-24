2fa2d1a664574673be37e4b3e7ce71300de0081a
Nitya020602 <nityagupta62@gmail.com>
2023-08-24 14:51:09 +0530

    comparison

--- src/components/addForm/index.js
+++ src/components/addForm/index.js
@@ -29,8 +29,9 @@ const AddForm=()=>{
         setHCategory(category)
         setOpen(false)
     }
+    console.log(expenditure,amt)
     const handleaction=()=>{
-        if (expenditure===(undefined||"")||amt===(undefined||"")||(!hcategory)){
+        if ((expenditure===undefined)||(expenditure==="")||(amt===undefined)||(amt==="")||(!hcategory)){
             const notify=()=>toast("Please re-enter details properly");
             notify();
             return;
@@ -67,7 +68,7 @@ const AddForm=()=>{
                 <div className="category">
                     <div className="categories-dropdown" onClick={()=>setOpen(!open)}>
                         <label>{hcategory?hcategory.title:"Category"}</label>
-                        <i class="fi fi-rr-angle-small-down"></i>
+                        <i class="fi fi-rr-angle-small-down icon"></i>
                     </div>
                     {open && <div className="categories-container">
                         {categorieslist.map(category=>(
